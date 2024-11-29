9d77a78ad8814faa6af4011e9384a374930061ed
Dan131195 <daniele.franceschini1395@gmail.com>
2024-11-29 16:27:50 +0100

    Esercizio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-const nav = document.querySelector('header');
+const nav = document.querySelector("header");
+const start = document.getElementById("start");
 
-nav.addEventListener('scroll', () => {
-    
-})
+nav.addEventListener("scroll", () => {
+  if (window.scrollY(590)) {
+    nav.style.backgroundColor = "White";
+    start.style.backgroundColor = "Green";
+  }
+});
